19ec78d7243ef30e56496234a15979fccc8ab403
Marcelo Mourier <marcelo_mourier@yahoo.com>
2023-01-28 16:06:55 -0700

    Safety commit

---
+++ defs.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <sys/queue.h>
+#include <sys/socket.h>
+#include <time.h>
+
+typedef struct CmdArgs {
+    int maxRiders;                      // Max number of riders that can join the group ride
+    int reportPeriod;                   // Period (in seconds) the client app needs to send its progress update messages
+    struct sockaddr_storage sockAddr;   // IP address and TCP port (in network byte order) used by GRS to listen for client connections
+    time_t startTime;                   // Start date/time (in UTC) for the group ride
+    int tcpPort;                        // TCP port used by the listening socket
+} CmdArgs;
+
+typedef enum Gender {
+    unspec = 0,
+    female = 1,
+    male = 2,
+    GenderMax = 3
+} Gender;
+
+typedef enum AgeGrp {
+    undef = 0,      // undefined
+    u19 = 1,        // 0-18
+    u35 = 2,        // 19-34
+    u40 = 3,        // 35-39
+    u45 = 4,        // 40-44
+    u50 = 5,        // 45-49
+    u55 = 6,        // 50-54
+    u60 = 7,        // 55-59
+    u65 = 8,        // 60-64
+    u70 = 9,        // 65-69
+    u75 = 10,       // 70-74
+    u80 = 11,       // 75-79
+    u85 = 12,       // 80-84
+    u90 = 13,       // 85-89
+    u95 = 14,       // 90-94
+    u100 = 15,      // 95-99
+    AgeGrpMax = 16
+} AgeGrp;
+
+// Rider
+typedef struct Rider {
+    int age;                            // rider's age (as of Dec 31 of the current year)
+    AgeGrp ageGrp;                      // rider's age group
+    Gender gender;                      // rider's gender
+    char *name;                         // rider's name or alias
+    time_t regTime;                     // time (UTC) the rider registered with the GRS
+    int sd;                             // file descriptor of the connected socket
+    struct sockaddr_storage sockAddr;   // remote IP address and TCP port
+} Rider;
+
+// Group Ride Server
+typedef struct Grs {
+    // List of registered riders
+    TAILQ_HEAD(RiderList, Rider) riderList[GenderMax][AgeGrpMax];
+    int sd;                             // file descriptor of the listening socket
+} Grs;
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
-#include "cmd_args.h"
+#include "defs.h"
 
 #define PROGRAM_VERSION     "0.0"
 
@@ -18,17 +20,35 @@ static const char *help =
         "        Show this help and exit.\n"
         "    --ip-addr <addr>\n"
         "        Specifies the IP address where the GRS app will listen for\n"
-        "        connections.\n"
+        "        connections. If no address is specified, the server will use\n"
+        "        any of the available network interfaces.\n"
         "    --max-riders <num>\n"
         "        Specifies the maximum number of riders allowed to join the\n"
         "        group ride.\n"
+        "    --report-period <secs>\n"
+        "        Specifies the period (in seconds) the client app's need to send\n"
+        "        their update messages to the server.\n"
         "    --start-time <time>\n"
-        "        Specifies the start time (UTC) of the group ride.\n"
+        "        Specifies the start date and time (in ISO 8601 UTC format) of\n"
+        "        the group ride.\n"
         "    --tcp-port <port>\n"
-        "        Specifies the TCP port used by the GRS app.\n"
+        "        Specifies the TCP port used by the GRS app. The default is TCP\n"
+        "        port 54321."
         "    --version\n"
         "        Show program's version info and exit.\n"
         "\n";
+
+static int invArg(const char *arg)
+{
+    fprintf(stderr, "Invalid argument: '%s'\n", arg);
+    return -1;
+}
+
+static int missArg(const char *arg, const char *val)
+{
+    fprintf(stderr, "Missing argument. Syntax: '%s %s'\n", arg, val);
+    return -1;
+}
 
 static int parseCmdArgs(int argc, char *argv[], CmdArgs *pArgs)
 {
@@ -36,7 +56,7 @@ static int parseCmdArgs(int argc, char *argv[], CmdArgs *pArgs)
 
     for (int n = 1; n <= numArgs; n++) {
         const char *arg;
-        //const char *val;
+        const char *val;
 
         arg = argv[n];
 
@@ -44,9 +64,57 @@ static int parseCmdArgs(int argc, char *argv[], CmdArgs *pArgs)
             fprintf(stdout, "%s\n", help);
             exit(0);
         } else if (strcmp(arg, "--ip-addr") == 0) {
-
+            val = argv[++n];
+            if (val == NULL) {
+                return missArg(arg, "<addr>");
+            } else {
+                struct in_addr addrV4;
+                struct in6_addr addrV6;
+                if (inet_pton(AF_INET, val, &addrV4) == 1) {
+                    struct sockaddr_in *sockAddr = (struct sockaddr_in *) &pArgs->sockAddr;
+                    sockAddr->sin_family = AF_INET;
+                    sockAddr->sin_addr = addrV4;
+                } else if (inet_pton(AF_INET6, val, &addrV6) == 1) {
+                    struct sockaddr_in6 *sockAddr = (struct sockaddr_in6 *) &pArgs->sockAddr;
+                    sockAddr->sin6_family = AF_INET6;
+                    sockAddr->sin6_addr = addrV6;
+                } else {
+                    return invArg(val);
+                }
+            }
+        } else if (strcmp(arg, "--max-riders") == 0) {
+            val = argv[++n];
+            if (val == NULL) {
+                return missArg(arg, "<num>");
+            } else if (sscanf(val, "%d", &pArgs->maxRiders) != 1) {
+                return invArg(val);
+            }
+        } else if (strcmp(arg, "--report-period") == 0) {
+            val = argv[++n];
+            if (val == NULL) {
+                return missArg(arg, "<num>");
+            } else if (sscanf(val, "%d", &pArgs->reportPeriod) != 1) {
+                return invArg(val);
+            }
         } else if (strcmp(arg, "--tcp-port") == 0) {
-
+            val = argv[++n];
+            if (val == NULL) {
+                return missArg(arg, "<num>");
+            } else if (sscanf(val, "%d", &pArgs->tcpPort) != 1) {
+                return invArg(val);
+            }
+        } else if (strcmp(arg, "--start-time") == 0) {
+            val = argv[++n];
+            if (val == NULL) {
+                return missArg(arg, "<date+time>");
+            } else {
+                struct tm brkDwnTime = {0};
+                if (strptime(val, "%Y-%m-%dT%H:%M:%S", &brkDwnTime) != NULL) {
+                    pArgs->startTime = mktime(&brkDwnTime);
+                } else {
+                    return invArg(val);
+                }
+            }
         } else if (strcmp(arg, "--version") == 0) {
             fprintf(stdout, "Program version %s built on %s %s\n", PROGRAM_VERSION, __DATE__, __TIME__);
             exit(0);
@@ -54,6 +122,24 @@ static int parseCmdArgs(int argc, char *argv[], CmdArgs *pArgs)
             fprintf(stderr, "Invalid option: %s\n", arg);
             return -1;
         }
+    }
+
+    // Sanity check the args..
+
+    if ((pArgs->tcpPort < 49152) || (pArgs->tcpPort > 65535)) {
+        return invArg("TCP port must be in the range 49152-65535");
+    }
+
+    // If no address was specified, use the IPv4 wildcard
+    if (pArgs->sockAddr.ss_family == 0) {
+        pArgs->sockAddr.ss_family = AF_INET;
+    }
+
+    // Set the TCP port in the socket address object
+    if (pArgs->sockAddr.ss_family == AF_INET) {
+        ((struct sockaddr_in *) &pArgs->sockAddr)->sin_port = htons(pArgs->tcpPort);
+    } else {
+        ((struct sockaddr_in6 *) &pArgs->sockAddr)->sin6_port = htons(pArgs->tcpPort);
     }
 
     return 0;
