1a732f488d5447b0d9bae1748b90b9f6df0c65ee
Marcelo Mourier <marcelo_mourier@yahoo.com>
2023-01-28 16:09:07 -0700

    Removed cmd_args.h

--- cmd_args.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <sys/socket.h>
-
-typedef struct CmdArgs {
-    struct sockaddr_storage sockAddr;   // IP address and TCP port used by GRS to listen for client connections
-} CmdArgs;
